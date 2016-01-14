8b3fdd8368c1c77f47ed806d2a10c602dc987ba5
think2011 <452125301@qq.com>
2016-01-14 21:51:18 +0800

    initial commi💡

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gulp-extract-path
-提取文件里面的文件路径，并存储另存为文件
+提取文件里面引用的文件
+
+## 示例
+
+```js
+.pipe(extractPath({rootPath: './src'}))
+```
---
+++ index.js
@@ -0,0 +1,31 @@
+const through = require('through2');
+const path    = require('path');
+
+/**
+ * 提取文件中引用的文件
+ * @param options
+ * @param options.rootPath 引用文件的根目录
+ *
+ * @example
+ * .pipe(extractPath({rootPath: './src'}))
+ * @returns {*}
+ */
+module.exports = function (options) {
+    return through.obj(function (file, enc, cb) {
+        let that = this;
+
+        if (file.isNull()) {
+            cb(null, file);
+        }
+
+        let content  = file.contents.toString();
+        let filesDir = content.match(/['|"].*\.(.*)['|"]/g).map((v) => {
+            return path.join(options.rootPath, v.substr(1, v.length - 2));
+        });
+
+        gulp.src(filesDir, {read: file, base: options.rootPath}).pipe(through.obj((file, enc, cb)=> {
+            that.push(file);
+            cb();
+        })).on('finish', cb)
+    });
+};
