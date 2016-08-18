0ade91b4c6f1b71b229fb73b14342b1c34e2f43e
think2011 <452125301@qq.com>
2016-08-18 16:56:53 +0800

    feat *

--- index.js
+++ index.js
@@ -6,37 +6,45 @@ const vinylFile = require('vinyl-file')
 
 /**
  * 提取文件中引用的文件
- * @param rootPath 引用文件的根目录
+ * @param options
+ * @param options.base
+ * @param [options.exts]
  *
  * @example
  * .pipe(extractPath('./src'))
  * @returns {*}
  */
-module.exports = function (rootPath) {
+module.exports = function (options) {
+    options = Object.assign({
+        exts: ['css', 'js', 'less']
+    }, options)
+
     return through.obj(function (file, enc, cb) {
         let that    = this
         let content = file.contents.toString()
-        let match   = content.match(/[^"'\s)]*\.(css|js)(?:'|")/g)
+        let reg     = new RegExp('[^"\')]*\.(css|js|less)(?:\'|")', 'g')
+        let match   = content.match(reg)
 
         if (file.isNull() || !match) {
-            return cb(null, file)
+            return cb()
         }
 
         let files = match
             .filter(v => !(v.startsWith('http') || v.startsWith('//')))
             .map(v => {
-                let filePath = path.join(process.cwd(), rootPath, v)
+                let filePath = path.join(options.base, v)
                 filePath     = filePath.substr(filePath, filePath.length - 1)
 
                 if (fs.existsSync(filePath)) {
                     let file  = vinylFile.readSync(filePath)
-                    file.base = path.join(process.cwd(), rootPath)
+                    file.base = path.resolve(process.cwd(), options.base)
 
                     return file
                 }
 
             })
 
+        // console.log(files)
         files.forEach((item) => item && that.push(item))
         cb()
     });
